ab52f81fef52f54fed5256874cbc6b964765f853
Infvyr <allfortrashmail@gmail.com>
2021-05-31 21:50:25 +0300

    configuring react-redux for users, connecting the currentUser with redux users store

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,32 +1,39 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils'
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-import './header.styles.scss'
+import './header.styles.scss';
 
-const Header = ({currentUser}) => (
-    <header className="header">
-        <Link to="/" className="logo-container">
-            <Logo className="logo" />
-        </Link>
-        <div className="options">
-            <Link to="/shop" className="option">SHOP</Link>
-            <Link to="/contact" className="option">CONTACT</Link>
-            {
-                currentUser 
-                ? 
-                (<div className="option" onClick={()=>auth.signOut()}>
-                    SIGN OUT
-                </div>)
-                :
-                (<Link to="/signin" className="option" onClick={signInWithGoogle}>
-                    {' '}
-                    SIGN IN{' '}
-                </Link>)
-            }
-        </div>
-    </header>
-)
+const Header = ({ currentUser }) => (
+	<header className="header">
+		<Link to="/" className="logo-container">
+			<Logo className="logo" />
+		</Link>
+		<div className="options">
+			<Link to="/shop" className="option">
+				SHOP
+			</Link>
+			<Link to="/contact" className="option">
+				CONTACT
+			</Link>
+			{currentUser ? (
+				<div className="option" onClick={() => auth.signOut()}>
+					SIGN OUT
+				</div>
+			) : (
+				<Link to="/signin" className="option" onClick={signInWithGoogle}>
+					{' '}
+					SIGN IN{' '}
+				</Link>
+			)}
+		</div>
+	</header>
+);
 
-export default Header;
+const mapSateToProps = (state) => ({
+	currentUser: state.user.currentUser,
+});
+
+export default connect(mapSateToProps)(Header);
